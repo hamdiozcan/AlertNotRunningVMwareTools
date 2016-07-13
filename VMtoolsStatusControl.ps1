abc6c35e6168a0f62c5352dc5f77a9490ec96735
Hamdi ÖZCAN <ahmethamdi@gmail.com>
2016-07-13 20:33:46 +0300

    Update and rename VDItoolsStatusControl.ps1 to VMtoolsStatusControl.ps1

--- VMtoolsStatusControl.ps1
+++ VMtoolsStatusControl.ps1
@@ -1,7 +1,7 @@
-# VDItoolsStatusControl
+# PowerShell VMtoolsStatusControl
 # July 2016 Hamdi OZCAN http://ozcan.com
 # task schedule 
-# C:\WINDOWS\system32\windowspowershell\v1.0\powershell.exe -file C:\PowerCLI\VDItoolsStatusControl.ps1
+# C:\WINDOWS\system32\windowspowershell\v1.0\powershell.exe -file C:\PowerCLI\VMtoolsStatusControl.ps1
 
 add-pssnapin VMware.VimAutomation.Core
 Connect-VIServer vcenterHostName
@@ -10,10 +10,10 @@ Connect-VIServer vcenterHostName
 [System.Array]$A2 = @()
 $OutputMail = ""
 
-$A2 = gc "C:\PowerCLI\VDItoolsStatusControl.txt"
+$A2 = gc "C:\PowerCLI\VMtoolsStatusControl.txt"
 if (!$A2) {$A2 += "placeholder"}
 
-ForEach ($VM in Get-Cluster VDI_CLUSTER_NAME | Get-VM | where-object {($_.extensiondata.Guest.ToolsStatus -eq "toolsNotRunning")} ) 
+ForEach ($VM in Get-Cluster CLUSTER_NAME | Get-VM | where-object {($_.extensiondata.Guest.ToolsStatus -eq "toolsNotRunning")} ) 
 {
 $A1 += $VM.Name
 }
@@ -21,16 +21,16 @@ $A1 += $VM.Name
 ForEach ($VM in Compare-Object $A1 $A2 | Where-Object { $_.SideIndicator -eq "<=" } ) 
 {
 $OutputMail = $OutputMail + $VM.InputObject + "`n"
-$VM.InputObject >> "C:\PowerCLI\VDItoolsStatusControl.txt"
+$VM.InputObject >> "C:\PowerCLI\VMtoolsStatusControl.txt"
 }
 
 ForEach ($VM in Compare-Object $A1 $A2 | Where-Object { $_.SideIndicator -eq "=>" } )
 {
 $A1.Remove($VM.InputObject)
-$A1 > "C:\PowerCLI\VDItoolsStatusControl.txt"
+$A1 > "C:\PowerCLI\VMtoolsStatusControl.txt"
 }
 
-if ($OutputMail) {send-mailmessage -to "mailme@mailme.com" -from "VDI VMwareTools <vcenter@vcenter.local>" -subject "VDI VMwareTools Not Running" -body $OutputMail -smtpServer mail.mailme.com}
+if ($OutputMail) {send-mailmessage -to "mailme@mailme.com" -from "VMwareTools <vcenter@vcenter.local>" -subject "VMwareTools Not Running" -body $OutputMail -smtpServer mail.mailme.com}
 
 Disconnect-VIServer -Force -Confirm:$false
 #Remove-PSSnapin -Name VMWare.VimAutomation.Core
